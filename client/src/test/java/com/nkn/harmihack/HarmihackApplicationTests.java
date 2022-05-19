c3b87de7b5f4db986f0cf03406790e7f41b09925
Mario Veelaid <maveel@ttu.ee>
2022-05-19 19:45:28 +0300

    Initial spring code

---
+++ client/src/test/java/com/nkn/harmihack/HarmihackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkn.harmihack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmihackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
