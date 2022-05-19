32b79a941945ae6df7d884eec94c798e368d0ad3
Lars Johannes Sissas <lars.johannes.sissas@ut.ee>
2022-05-19 20:28:58 +0300

    installed sass

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -5,3 +5,13 @@
     <h1>Harmihack</h1>
   </div>
 </template>
+
+<style lang="scss">
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+</style>
